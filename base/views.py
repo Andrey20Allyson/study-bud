c17ef896681593aa2bbea0eb779bc0a8d77adb46
Andrey Allyson <andreyuser47@gmail.com>
2024-12-13 17:12:07 -0300

    python course (setup, views and models)

---
+++ base/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Room
+
+# Create your views here.
+
+def home(req):
+    rooms = Room.objects.all()
+    context = { 'rooms': rooms }
+
+    return render(req, 'base/home.html', context)
+
+def room(req, pk):
+    room = Room.objects.get(id=pk)
+    
+    context = { 'room': room }
+
+    return render(req, 'base/room.html', context)
